81158c03d8eab57fcc21a2dd580c5f08e8ced002
abusedmedia <hello@fabiofranchino.com>
2023-08-09 10:32:50 +0300

    pkg updated

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,8 @@ import commonjs from '@rollup/plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
 import autoprefixer from 'autoprefixer'
 import cssnano from 'cssnano'
+import { terser } from 'rollup-plugin-minification'
 
-const terser = require('rollup-plugin-terser').terser
 
 export default {
   input: 'src/index.js',
